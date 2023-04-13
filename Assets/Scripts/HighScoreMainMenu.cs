48d44a97f12510ac9e2902d14ff1c291d138e005
Jacky Lee <u3578552@connect.hku.hk>
2023-04-14 01:01:16 +0800

    feat: show resume round num in main menu

--- Assets/Scripts/HighScoreMainMenu.cs
+++ Assets/Scripts/HighScoreMainMenu.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
-using BayatGames.SaveGameFree; 
+using BayatGames.SaveGameFree;
 using TMPro;
+using BayatGames.SaveGameFree;
 
 public class HighScoreMainMenu : MonoBehaviour
 {
     [SerializeField] public TextMeshProUGUI highScoreUI;
     [SerializeField] public TextMeshProUGUI highScoreTimeUI;
+    [SerializeField] public TextMeshProUGUI resumeUI;
 
 
     // Start is called before the first frame update
@@ -16,27 +18,40 @@ public class HighScoreMainMenu : MonoBehaviour
     {
         highScoreUI = gameObject.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
         highScoreTimeUI = gameObject.transform.GetChild(1).GetComponent<TextMeshProUGUI>();
-        int highScoreRound =  SaveGame.Load<int>("round");
+        resumeUI = gameObject.transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+        int highScoreRound = SaveGame.Load<int>("round");
         int highScore = SaveGame.Load<int>("highScore");
         String highScoreTime = SaveGame.Load<String>("highScoreTime");
         Debug.Log("high score in main menu: " + highScore);
-        if (highScore > 0 ){
+        if (highScore > 0)
+        {
             highScoreUI.text = "High Score: " + highScore + " In " + highScoreRound + " Rounds";
             highScoreTimeUI.text = "On: " + highScoreTime;
             // GUIUtility.systemCopyBuffer = "HighScore:" + highScore + " on " +  highScoreTime;
         }
-        else{
+        else
+        {
             highScoreUI.text = "High Score: 0";
+        }
+
+        if (SaveGame.Load<int>("round") > 0)
+        {
+            resumeUI.text = "Continue: You left on Round " + SaveGame.Load<int>("round");
+        }
+        else
+        {
+            resumeUI.text = "";
         }
     }
 
-    public void copyToClipboard(){
-        GUIUtility.systemCopyBuffer = "HighScore:" + highScoreUI.text + " on " +  highScoreTimeUI.text;
+    public void copyToClipboard()
+    {
+        GUIUtility.systemCopyBuffer = "HighScore:" + highScoreUI.text + " on " + highScoreTimeUI.text;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
