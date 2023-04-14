cee8c86d70c1507539e595e95ea697777a8aafdd
Olano James Daniel Cubillas <u3577797@connect.hku.hk>
2023-04-15 01:39:27 +0800

    Added popup text for barricade purchase

--- Assets/Scripts/Barricade.cs
+++ Assets/Scripts/Barricade.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
+using TMPro;
 
 public class Barricade : MonoBehaviour
 {
     public int barricadeCost = 10;
     public FirstPersonController fpsc;
     public LayerMask mask; 
+    [SerializeField] public GameObject textMesh;
     private bool isLookingAtBarricade = false;
 
+
+private void Start()
+    {
+        textMesh.GetComponent<TextMeshProUGUI>().text = "";
+    }
 void Update()
     {
         Debug.Log(isLookingAtBarricade);
         if (isLookingAtBarricade && Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("123WTF");
             if (fpsc.currency >= barricadeCost)
             {
                 fpsc.currency -= barricadeCost;
@@ -24,7 +30,7 @@ void Update()
             else
             {
                 // Player can't afford the weapon, so display a message
-                Debug.Log("You can't afford this barricade!");
+                textMesh.GetComponent<TextMeshProUGUI>().text = "Not enough funds";
             }
         }
     }
@@ -38,13 +44,22 @@ void Update()
             Debug.Log(obj.name);
             if(obj.name == "B_PurchaseProximity" || obj.name == "Barricades"){
                 isLookingAtBarricade = true;
+                textMesh.GetComponent<TextMeshProUGUI>().text = "Press <E> to purchase for $"+barricadeCost;
             }
-            else
+            else{
+                Debug.Log("wtf?");
                 isLookingAtBarricade = false;
+                textMesh.GetComponent<TextMeshProUGUI>().text = "";
+            }
+        }
+        else
+        {
+            textMesh.GetComponent<TextMeshProUGUI>().text = "";
         }
     }
     void OnTriggerExit(Collider other)
     {
+        textMesh.GetComponent<TextMeshProUGUI>().text = "";
         if (other.tag == "Player")
         {
             isLookingAtBarricade = false;
