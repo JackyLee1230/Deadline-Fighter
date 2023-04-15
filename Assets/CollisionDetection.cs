88688a324076ae25a22d654e3800bb536033f0c9
h3578661 <junchen4@connect.hku.hk>
2023-04-15 21:00:20 +0800

    fix: crit damage mechanic

--- Assets/CollisionDetection.cs
+++ Assets/CollisionDetection.cs
@@ -18,22 +18,37 @@ public class CollisionDetection : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         if(!damageCooldown && other.tag == "Zombie" && Melee.isAttacking){
-                damageCooldown = true;
-                StartCoroutine(ResetDamageCooldown());
+            damageCooldown = true;
+            StartCoroutine(ResetDamageCooldown());
+            if(!other.transform.root.GetComponent<AIExample>().isAware)
+            {
                 if (Melee.isCrit)
                 {
-                damage *= 2;
+                    other.transform.root.GetComponent<AIExample>().onHit(damage * stealthMultiplier*2);
                 }
-                if(!other.transform.root.GetComponent<AIExample>().isAware){
-                    other.transform.root.GetComponent<AIExample>().onHit(damage*stealthMultiplier);
-                }else{
+                else
+                {
+                    other.transform.root.GetComponent<AIExample>().onHit(damage * stealthMultiplier);
+                }
+                    
+             }
+            else
+            {
+                if (Melee.isCrit)
+                {
+                    other.transform.root.GetComponent<AIExample>().onHit(damage*2);
+                }
+                else
+                {
                     other.transform.root.GetComponent<AIExample>().onHit(damage);
                 }
-
-                Instantiate(HitParticle, new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z), other.transform.rotation);
-
-                Instantiate(damageEffect, effectPoint.transform.position, Quaternion.identity);
+                    
             }
+
+            Instantiate(HitParticle, new Vector3(other.transform.position.x, transform.position.y, other.transform.position.z), other.transform.rotation);
+
+            Instantiate(damageEffect, effectPoint.transform.position, Quaternion.identity);
+        }
     }
 
     IEnumerator ResetDamageCooldown()
